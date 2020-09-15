dd83b1fae16c5545b14dc0511516a9b5849cd83c
Tejas Mehta <tmthecoder@gmail.com>
2020-09-15 19:27:16 -0400

    add a config file

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ultimategoal/shared/RobotConfig.java
@@ -0,0 +1,9 @@
+/****
+ * Made by Tejas Mehta
+ * Made on Tuesday, September 15, 2020
+ * File Name: RobotConfig
+ * Package: org.firstinspires.ftc.teamcode.ultimategoal.shared*/
+package org.firstinspires.ftc.teamcode.ultimategoal.shared;
+
+public class RobotConfig {
+}
