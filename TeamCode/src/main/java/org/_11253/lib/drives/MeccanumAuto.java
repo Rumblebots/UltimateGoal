6f2b76c738e6e8b7a75a19345ce15a147daac74d
Tejas Mehta <tmthecoder@gmail.com>
2020-09-29 20:20:33 -0400

    meeccanum auto implementation
    Signed-off-by: Tejas Mehta <tmthecoder@gmail.com>

---
+++ TeamCode/src/main/java/org/_11253/lib/drives/MeccanumAuto.java
@@ -0,0 +1,54 @@
+/****
+ * Made by Tejas Mehta
+ * Made on Tuesday, September 29, 2020
+ * File Name: MeccanumAuto
+ * Package: org._11253.lib.drives*/
+package org._11253.lib.drives;
+
+import org._11253.lib.motors.MotorPower;
+import org._11253.lib.op.Auton;
+import org._11253.lib.robot.phys.assm.Drivetrain;
+import org._11253.lib.utils.telem.Telemetry;
+
+enum DriveDirection {
+    FORWARD,
+    BACKWARD,
+    LEFT,
+    RIGHT,
+}
+
+public class MeccanumAuto extends Auton {
+    public Drivetrain drivetrain = new Drivetrain();
+
+    public void meccanumDrive(DriveDirection direction, double power) {
+        switch (direction) {
+            case FORWARD:
+                drivetrain.setPower(new MotorPower(power));
+                break;
+            case BACKWARD:
+                drivetrain.setPower(new MotorPower(-power));
+                break;
+            case LEFT:
+                drivetrain.setPower(new MotorPower(power, -power, -power, power));
+                break;
+            case RIGHT:
+                drivetrain.setPower(new MotorPower(-power, power, power, -power));
+                break;
+        }
+    }
+
+    public MeccanumAuto() {
+        beforeStart.add(new Runnable() {
+            @Override
+            public void run() {
+                drivetrain.init();
+            }
+        });
+        onStart.add(new Runnable() {
+            @Override
+            public void run() {
+                Telemetry.addData("Autonomous", "Running Mecannum Autonomous...", "");
+            }
+        });
+    }
+}
