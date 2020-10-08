a4b47b3a877abeb6c73898f65a538d2124e66e59
Wobblyyyy <wobblyyyy@gmail.com>
2020-10-08 19:17:28 -0400

    delete un-needed file

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ultimategoal/shared/RobotConfig.java
+++
@@ -1,9 +0,0 @@
-/****
- * Made by Tejas Mehta
- * Made on Tuesday, September 15, 2020
- * File Name: RobotConfig
- * Package: org.firstinspires.ftc.teamcode.ultimategoal.shared*/
-package org.firstinspires.ftc.teamcode.ultimategoal.shared;
-
-public class RobotConfig {
-}
