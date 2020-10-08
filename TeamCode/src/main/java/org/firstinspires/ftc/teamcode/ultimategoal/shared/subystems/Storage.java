014a945a21c2b2fa3c3f3da27447b1051c87b7e3
Wobblyyyy <wobblyyyy@gmail.com>
2020-10-08 19:09:34 -0400

    some small corrections the shooter thingy

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ultimategoal/shared/subystems/Storage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ultimategoal/shared/subystems/Storage.java
@@ -1,21 +1,36 @@
 package org.firstinspires.ftc.teamcode.ultimategoal.shared.subystems;
 
+import org._11253.lib.utils.telem.Telemetry;
+
 public class Storage {
     private static int count = 0;
 
     public static void increment() {
-        count++;
+        if (count++ > 3) count = 3;
     }
 
     public static void decrement() {
-        count--;
+        if (count-- < 0) count = 0;
     }
 
     public static int getCount() {
         return count;
     }
 
+    public static boolean canShoot() {
+        return count - 1 < 0;
+    }
+
     public static boolean canIntake() {
         return count + 1 > 3;
+    }
+
+    public static void showCount() {
+        Telemetry.addData(
+                "Storage_ring_count",
+                "Rings in storage",
+                ":",
+                String.valueOf(count)
+        );
     }
 }
