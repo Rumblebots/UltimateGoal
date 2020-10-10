e38a1f2724f7746bf9f16abfe5e26a8f13944e81
colin robertson <wobblyyyy@gmail.com>
2020-10-10 19:03:50 -0400

    add settings to template (kinda)

--- TeamCode/src/main/java/org/_11253/lib/op/Template.java
+++ TeamCode/src/main/java/org/_11253/lib/op/Template.java
@@ -36,6 +36,7 @@ import org._11253.lib.utils.math.Math;
 import org._11253.lib.utils.telem.Telemetry;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Template LinearOpMode which extends the default (FTC) LinearOpMode.
@@ -51,41 +52,161 @@ import java.util.ArrayList;
 public class Template extends LinearOpMode {
     public int executionTime = 0;
     public double avgExecTime = 1;
-
     public Telemetry telem;
     public StringEvents events = new StringEvents();
-
     /**
      * List of Runnables to be run before the start of the
      * operation mode. This is before waitForStart() finishes;
      */
     public ListWrapper<Runnable> beforeStart = new ListWrapper<>(new ArrayList<Runnable>());
-
     /**
      * List of Runnables to be run after waitForStart() finishes.
      */
     public ListWrapper<Runnable> onStart = new ListWrapper<>(new ArrayList<Runnable>());
-
     /**
      * List of Runnables to run at the start of the loop.
      */
     public ListWrapper<Runnable> onStartRun = new ListWrapper<>(new ArrayList<Runnable>());
-
     /**
      * List of Runnables to run as the main portion of the loop.
      */
     public ListWrapper<Runnable> run = new ListWrapper<>(new ArrayList<Runnable>());
-
     /**
      * List of Runnables to run after the loop finishes.
      */
     public ListWrapper<Runnable> onFinishRun = new ListWrapper<>(new ArrayList<Runnable>());
-
     /**
      * List of Runnables to be run after the loop finishes being, well,
      * looped.
      */
     public ListWrapper<Runnable> onFinish = new ListWrapper<>(new ArrayList<Runnable>());
+
+    /**
+     * Should we send telemetry to the driver station's
+     * phone detailing how long each repitition of the
+     * default operation loop takes?
+     * {@link #applySettings(HashMap)}
+     */
+    private boolean showExecutionTime = true;
+    /**
+     * Should buttons be mapped automatically? IE.
+     * telemetry mapping the start button?
+     * {@link #applySettings(HashMap)}
+     */
+    private boolean automaticButtonMapping = true;
+    /**
+     * Should beforeStart list be run?
+     * {@link #applySettings(HashMap)}
+     */
+    private boolean runBeforeStart = true;
+    /**
+     * Should onStart list be run?
+     * {@link #applySettings(HashMap)}
+     */
+    private boolean runOnStart = true;
+    /**
+     * Should onStartRun list be run?
+     * {@link #applySettings(HashMap)}
+     */
+    private boolean runOnStartRun = true;
+    /**
+     * Should run list be run?
+     * {@link #applySettings(HashMap)}
+     */
+    private boolean runRun = true;
+    /**
+     * Should onFinishRun list be run?
+     * {@link #applySettings(HashMap)}
+     */
+    private boolean runOnFinishRun = true;
+    /**
+     * Should onFinish list be run?
+     * {@link #applySettings(HashMap)}
+     */
+    private boolean runOnFinish = true;
+    /**
+     * Should ticks be executed at some certain time,
+     * or once every single loop? This should (generally)
+     * be false.
+     * {@link #applySettings(HashMap)}
+     */
+    private boolean tickAsynchronously = false;
+    /**
+     * Should ticks be executed every loop of the
+     * operation loop? This should (generally)
+     * be true.
+     * {@link #applySettings(HashMap)}
+     */
+    private boolean tickOnRun = true;
+    /**
+     * Should telemetry be updated every certain
+     * amount of time? This should (generally)
+     * be true.
+     * {@link #applySettings(HashMap)}
+     */
+    private boolean updateTelemetryAsynchronously = true;
+    /**
+     * Should telemetry be updated every loop of
+     * the operation loop? This should (generally)
+     * be false.
+     * {@link #applySettings(HashMap)}
+     */
+    private boolean updateTelemetryOnRun = false;
+
+    /**
+     * Can be used to modify settings. This isn't
+     * entirely needed and doesn't really do much,
+     * but it provides an extra degree of information...
+     * just in case you'd ever want it.
+     *
+     * @param settings a HashMap containing settings
+     */
+    public void applySettings(HashMap<String, Boolean> settings) {
+        for (HashMap.Entry<String, Boolean> entry : settings.entrySet()) {
+            String name = entry.getKey();
+            boolean setting = entry.getValue();
+            switch (name) {
+                case "showExecutionTime":
+                    showExecutionTime = setting;
+                    break;
+                case "automaticButtonMapping":
+                    automaticButtonMapping = setting;
+                    break;
+                case "runBeforeStart":
+                    runBeforeStart = setting;
+                    break;
+                case "runOnStart":
+                    runOnStart = setting;
+                    break;
+                case "runOnStartRun":
+                    runOnStartRun = setting;
+                    break;
+                case "runRun":
+                    runRun = setting;
+                    break;
+                case "runOnFinishRun":
+                    runOnFinishRun = setting;
+                    break;
+                case "runOnFinish":
+                    runOnFinish = setting;
+                    break;
+                case "tickAsynchronously":
+                    tickAsynchronously = setting;
+                    break;
+                case "tickOnRun":
+                    tickOnRun = setting;
+                    break;
+                case "updateTelemetryAsynchronously":
+                    updateTelemetryAsynchronously = setting;
+                    break;
+                case "updateTelemetryOnRun":
+                    updateTelemetryOnRun = setting;
+                    break;
+                default:
+                    throw new IllegalArgumentException("Invalid setting!");
+            }
+        }
+    }
 
     /**
      * Run all of the runnables contained in a list.
@@ -102,42 +223,48 @@ public class Template extends LinearOpMode {
      * Internal function, just don't mess with it and you're good.
      */
     private void fBeforeStart() {
-        runList(beforeStart);
+        if (runBeforeStart)
+            runList(beforeStart);
     }
 
     /**
      * Internal function, just don't mess with it and you're good.
      */
     private void fOnStart() {
-        runList(onStart);
+        if (runOnStart)
+            runList(onStart);
     }
 
     /**
      * Internal function, just don't mess with it and you're good.
      */
     private void fOnStartRun() {
-        runList(onStartRun);
+        if (runOnStartRun)
+            runList(onStartRun);
     }
 
     /**
      * Internal function, just don't mess with it and you're good.
      */
     private void fRun() {
-        runList(run);
+        if (runRun)
+            runList(run);
     }
 
     /**
      * Internal function, just don't mess with it and you're good.
      */
     private void fOnFinishRun() {
-        runList(onFinishRun);
+        if (runOnFinishRun)
+            runList(onFinishRun);
     }
 
     /**
      * Internal function, just don't mess with it and you're good.
      */
     private void onFinish() {
-        runList(onFinish);
+        if (runOnFinish)
+            runList(onFinish);
     }
 
     /**
@@ -175,30 +302,40 @@ public class Template extends LinearOpMode {
     public final void runOpMode() {
         Global.setHwMap(hardwareMap);
         Global.setTelem(telemetry);
+
         telem = new Telemetry();
 
         fBeforeStart();
 
         waitForStart();
 
-        Telemetry.scheduleTelemetryUpdater();
+        if (updateTelemetryAsynchronously)
+            Telemetry.scheduleTelemetryUpdater();
 
         fOnStart();
 
         while (opModeIsActive()) {
             executionTime = (int) System.currentTimeMillis();
+
             fOnStartRun();
 
             fRun();
 
             fOnFinishRun();
+
             executionTime = (int) System.currentTimeMillis() - executionTime;
             avgExecTime = Math.average(executionTime, avgExecTime);
-            Telemetry.addData("_1125c_AVG_EXEC_TIME",
-                    "Average execution time",
-                    (Math.round(avgExecTime * 100) / 100) + "ms");
 
-            StringEvents.tick();
+            if (showExecutionTime)
+                Telemetry.addData("_1125c_AVG_EXEC_TIME",
+                        "Average execution time",
+                        (Math.round(avgExecTime * 100) / 100) + "ms");
+
+            if (updateTelemetryOnRun)
+                Telemetry.printTelemetry();
+
+            if (tickOnRun)
+                StringEvents.tick();
         }
 
         fOnFinishRun();
