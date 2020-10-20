f8b84356340abf5829e2a3cc2f947ee573e57323
Tejas Mehta <tmthecoder@gmail.com>
2020-10-20 19:05:18 -0400

    Add odometry wheels class
    Signed-off-by: Tejas Mehta <tmthecoder@gmail.com>

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ultimategoal/auton/GenericAuto.java
@@ -0,0 +1,22 @@
+/****
+ * Made by Tejas Mehta
+ * Made on Thursday, October 15, 2020
+ * File Name: AutoRed
+ * Package: org.firstinspires.ftc.teamcode.ultimategoal.auton*/
+package org.firstinspires.ftc.teamcode.ultimategoal.auton;
+
+import org._11253.lib.op.Auton;
+
+public class GenericAuto extends Auton {
+
+    public void initializeOdometry(int pos) {
+        //Initialize odometry with the given values
+        beforeStart.add(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+    }
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ultimategoal/shared/subystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ultimategoal/shared/subystems/Intake.java
@@ -2,7 +2,8 @@
  * Made by Tejas Mehta
  * Made on Thursday, October 08, 2020
  * File Name: Intake
- * Package: org._11253.lib.robot.phys.assm*/
+ * Package: org._11253.lib.robot.phys.assm
+ */
 package org.firstinspires.ftc.teamcode.ultimategoal.shared.subystems;
 
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
@@ -12,7 +13,7 @@ import org._11253.lib.robot.phys.components.Motor;
 /***
  * @author Tejas Mehta
  * Simple class for communicating with a two-motor intake
- * Motors should be faced in opposite directions, hence the setting of a revered & forward durction on each
+ * Motors should be faced in opposite directions, hence the setting of a * Motors should be faced in opposite directions, hence the setting of a revered & forward durction on each revered & forward durction on each
  */
 
 public class Intake {
