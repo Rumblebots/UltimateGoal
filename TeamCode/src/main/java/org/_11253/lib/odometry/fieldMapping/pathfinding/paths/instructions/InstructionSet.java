be78f7cbfde43fda78f1012331fae3eaa314afcb
Wobblyyyy <wobblyyyy@gmail.com>
2020-10-21 21:58:18 -0400

    i have to do stuff AH

---
+++ TeamCode/src/main/java/org/_11253/lib/odometry/fieldMapping/pathfinding/paths/instructions/InstructionSet.java
@@ -0,0 +1,6 @@
+package org._11253.lib.odometry.fieldMapping.pathfinding.paths.instructions;
+
+import org._11253.lib.utils.jrep.ListWrapper;
+
+public class InstructionSet<E> extends ListWrapper<E> {
+}
